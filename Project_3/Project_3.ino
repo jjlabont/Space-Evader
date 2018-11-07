c35d3861c38fa7af41339dcf4e4adc168d8b15f3
jjlabont <jjlabont@buffalo.edu>
2018-11-07 11:46:28 -0500

    Trying to make some renderer stuff

--- Project_3/Project_3.ino
+++ Project_3/Project_3.ino
@@ -12,20 +12,22 @@
 Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
 uint8_t yPos = 12;
-int up = 5;
-int down = 4;
+int up = 4;
+int down = 5;
+int fire = 3;
 
-void setup()   { 
+void setup()   {
   pinMode(up, INPUT_PULLUP);
   pinMode(down, INPUT_PULLUP);
+  pinMode(fire, INPUT_PULLUP);
   Serial.begin(9600);
-  
+
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
   display.begin(SSD1306_SWITCHCAPVCC);
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  display.setCursor(0,0);
+  display.setCursor(0, 0);
   display.println("Hello world!");
   display.display();
   delay(1000);
@@ -52,6 +54,37 @@ void shipMove() {
 class enitity
 {
   public:
-  uint8_t xPosition;
-  uint8_t yPosition;
+    uint8_t xPosition = 0;
+    uint8_t yPosition = 0;
+    int sprt = 16;
+
+    entity(uint8_t xPos, uint8_t yPos, int sprite) {
+      xPosition = xPos;
+      yPosition = yPos;
+      sprt = sprite;
+    }
+};
+
+class renderer
+{
+  public:
+    entity* sprites[20];
+    numSprites = 0;
+
+    renderer() {
+    }
+
+    void addEntity(entity& ent) {
+      sprites[numSprites] = ent;
+      numSprites += 1;
+    }
+
+    void update() {
+      for (int i = 0; i < numSprites; i++) {
+        uint8_t xPos = sprites[i].xPosition;
+        uint8_t yPox = sprites[i].yPosition;
+
+      }
+    }
+
 };
